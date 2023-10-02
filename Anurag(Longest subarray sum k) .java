5971cb9fd2b74a10df67e8bdcfd4dcf949b96ce3
Anurag Singh Rajput <145913972+Daya1511@users.noreply.github.com>
2023-10-02 15:58:41 +0530

    Update Anurag(Longest subarray sum k) .java

--- Anurag(Longest subarray sum k) .java
+++ Anurag(Longest subarray sum k) .java
@@ -24,3 +24,35 @@ public class Solution {
         return max;
     }
 }
+// using hashamp...........
+
+import java.util.HashMap;
+public class Solution {
+    public static int longestSubarrayWithSumK(int []a, long k) {
+        // Write your code here
+        HashMap<Long,Integer> map=new HashMap<>();
+        int n=a.length;
+        int max=0;
+        long sum=0;
+        for(int i=0;i<n;i++)
+        {
+          sum+=a[i];
+          if(sum==k)
+          {
+              max=Math.max(max,i+1);
+          }
+          long key=sum-k;
+          if(map.containsKey(key))
+          {
+              int length=i-map.get(key);
+              max=Math.max(max,length);
+          }
+          if(!map.containsKey(sum))
+          {
+          map.put(sum,i);
+          }
+          
+        }
+        return max;
+    }
+}
