37713a3418865a79ec86b359f9930c3d5e1f7d6f
Anurag Singh Rajput <145913972+Daya1511@users.noreply.github.com>
2023-10-02 16:10:52 +0530

    Create Anurag(leader in array).java

---
+++ Anurag(leader in array).java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class Solution {
+    public static List< Integer > superiorElements(int []a) {
+        // Write your code here.
+        ArrayList<Integer> list=new ArrayList<>();
+        int n=a.length;
+       int max = a[n - 1];
+
+       list.add(a[n-1]);
+
+        for(int i=n-2;i>=0;i--)
+        {
+           if (a[i] > max)
+            {
+             list.add(a[i]);
+             max = a[i];
+            }
+               
+        }
+       
+        return list;
+    }
+}
